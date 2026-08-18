1132e927e0de88e6bef5a8d134a854e4a2f9502c
Thiago Leite <thiagofmleite@gmail.com>
2026-08-18 12:49:56 -0300

    Refactor event handling and configuration structure; add event bus implementation and tests

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -3,15 +3,23 @@ package app
 import (
 	"log"
 	"time"
+
+	"github.com/thiagoleet/kiosk-home-display/internal/events"
 )
 
-type App struct{}
+type App struct {
+	bus *events.Bus
+}
 
 func New() *App {
-	return &App{}
+	return &App{
+		bus: events.NewBus(),
+	}
 }
 
 func (a *App) Run() error {
+	a.registerHandlers()
+
 	log.Println("Kiosk Home Display backend is running")
 
 	for {
@@ -19,4 +27,10 @@ func (a *App) Run() error {
 	}
 
 	return nil
+}
+
+func (a *App) registerHandlers() {
+	a.bus.Subscribe(events.EventNotification, func(event events.Event) {
+		log.Printf("Notification received: %v", event.Data)
+	})
 }
